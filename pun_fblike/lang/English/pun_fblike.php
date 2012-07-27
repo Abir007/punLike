59e938c9e151f02973bed37512d1083cd94b5443
Abir Sarkar <abirsarkarofficial@gmail.com>
2012-07-27 15:38:27 +0530

    punBB Extension Release

---
+++ pun_fblike/lang/English/pun_fblike.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('FORUM')) die();
+
+$lang_pun_fblike = array(
+	'Setup'						=> 'Add Facebook Like',
+	
+	'Domain'					=> 'Your Domain',
+	'Domain help'				=> 'Your Domain (eg. studyforums.org)',
+	
+	'Type'						=> 'Enable/Disable',
+	'Type disable'					=> 'disable',
+	'Type enable'					=> 'enable'
+)
+
+?>
